4b7a0bafbaf4ee5be41471d662b3ee0057cad16d
karanara <karanara@mail.uc.edu>
2024-02-12 00:01:47 -0500

    multithreading using java 8 Executor Service of odd even

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,3 +1,8 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
 import javax.management.ObjectName;
 
 public class EvenOdd implements Runnable{
@@ -35,12 +40,33 @@ public class EvenOdd implements Runnable{
 
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Object obj = new Object();
         Runnable r1 = new EvenOdd(obj);
         Runnable r2 = new EvenOdd(obj);
         new Thread(r1,"even").start();
         new Thread(r2,"odd").start();
+    }*/
+    public static void main (String[] args){
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        IntStream.rangeClosed(1, 10)
+        .forEach(num->{
+            CompletableFuture<Integer> oddCompletableFuture = CompletableFuture.completedFuture(num)
+            .thenApplyAsync(x->{
+                if (x%2!=0) {
+                    System.out.println("Thread Name " + Thread.currentThread().getName() + " : " + x);
+                }
+                return num; },executorService);
+                oddCompletableFuture.join();
+                CompletableFuture<Integer> EvenCompletableFuture = CompletableFuture.completedFuture(num)
+                .thenApplyAsync(x->{
+                    if (x%2==0) {
+                        System.out.println("Thread Name " + Thread.currentThread().getName() + " : " + x);
+                    }
+                    return num; },executorService);
+                    EvenCompletableFuture.join();
+            
+            });
     }
     
 }
